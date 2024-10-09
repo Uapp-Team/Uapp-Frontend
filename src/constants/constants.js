ccb66afbfc48c08109058cc4094ea973ea35b507
MohammadSiam <siamk417@gmail.com>
2024-10-09 12:11:57 +0600

    fixed package json file for different purpose

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,18 @@
-// export const rootUrl = process.env.REACT_APP_ROOT_URL_LOCAL;
-export const rootUrl = process.env.REACT_APP_ROOT_URL_TEST;
-// export const rootUrl = process.env.REACT_APP_ROOT_URL_PROD;
-//export const rootUrl = process.env.REACT_APP_ROOT_URL;
+const environment = process.env.REACT_APP_ENV;
+
+let rootUrl;
+switch (environment) {
+    case 'dev':
+        rootUrl = process.env.REACT_APP_ROOT_URL_DEV;
+        break;
+    case 'local':
+        rootUrl = process.env.REACT_APP_ROOT_URL_LOCAL;
+        break;
+    case 'prod':
+        rootUrl = process.env.REACT_APP_ROOT_URL_PROD;
+        break;
+    default:
+        rootUrl = process.env.REACT_APP_ROOT_URL;
+}
+
+export { rootUrl };
