3a3a77e8ece0dd2fff716586daa4e7ed4685f3d1
Mahedi <bluebaymahedi@gmail.com>
2024-11-07 17:54:38 +0600

    update

--- src/views/SMS/AllStaffs/Staffs/StaffsAllInformation/GeneralInformation/Index.js
+++ src/views/SMS/AllStaffs/Staffs/StaffsAllInformation/GeneralInformation/Index.js
@@ -19,9 +19,6 @@ const StaffGeneralInformation = () => {
   const [parentValue, setParentValue] = useState(0);
   const [typeLabel, setTypeLabel] = useState("Select Staff Type");
   const [typeValue, setTypeValue] = useState(0);
-  const [homeAccept, setHomeAccept] = useState(false);
-  const [ukAccept, setUkAccept] = useState(false);
-  const [intAccept, setIntAccept] = useState(false);
   const [branch, setBranch] = useState([]);
   const [branchLabel, setBranchLabel] = useState("London office");
   const [branchValue, setBranchValue] = useState(1);
@@ -74,9 +71,6 @@ const StaffGeneralInformation = () => {
       setBranchLabel(res?.branch?.name);
       setTitleValue(res?.nameTittle?.id);
       setNameLabel(res?.nameTittle?.name);
-      setHomeAccept(res?.isAcceptedHome);
-      setUkAccept(res?.isAcceptedEU_UK);
-      setIntAccept(res?.isAcceptedInternational);
       setFirstName(res?.firstName);
       setLastName(res?.lastName);
       setEmail(res?.email);
@@ -157,10 +151,6 @@ const StaffGeneralInformation = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const subdata = new FormData(event.target);
-
-    subdata.append("isAcceptedHome", homeAccept);
-    subdata.append("isAcceptedEU_UK", ukAccept);
-    subdata.append("isAcceptedInternational", intAccept);
 
     for (var value of subdata) {
       console.log(value);
