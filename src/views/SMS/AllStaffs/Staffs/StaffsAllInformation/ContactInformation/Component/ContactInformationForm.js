0e2fabfb37924c69141171f9238d15064bba67ad
MohammadSiam <siamk417@gmail.com>
2024-12-23 16:59:05 +0600

    fixed contact information address issue

--- src/views/SMS/AllStaffs/Staffs/StaffsAllInformation/ContactInformation/Component/ContactInformationForm.js
+++ src/views/SMS/AllStaffs/Staffs/StaffsAllInformation/ContactInformation/Component/ContactInformationForm.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Col, FormGroup, Input, Row } from "reactstrap";
 import Select from "react-select";
+import { Col, FormGroup, Input, Row } from "reactstrap";
 import PreviousButton from "../../../../../../../components/buttons/PreviousButton";
 import SaveButton from "../../../../../../../components/buttons/SaveButton";
 import { permissionList } from "../../../../../../../constants/AuthorizationConstant";
@@ -139,7 +139,7 @@ const ContactInformationForm = ({
           <Input
             type="text"
             id="address"
-            placeholder="Enter house no."
+            placeholder="Enter Address"
             onChange={(e) => {
               handleHouse(e);
             }}
