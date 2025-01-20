a2f00d03c1e04d755c39f6d473deba8570fa3982
bbmahedi <bluebaymahedi@gmail.com>
2025-01-20 16:20:19 +0600

    `Added StatusCard component to QualityReport view and updated style.css`

--- src/views/SMS/ApplicationPipeline/QualityReport.js
+++ src/views/SMS/ApplicationPipeline/QualityReport.js
@@ -1,10 +1,18 @@
 import React from "react";
 import BreadCrumb from "../../../components/breadCrumb/BreadCrumb";
+import StatusCard from "./Components/StatusCard";
+import { Col, Row } from "reactstrap";
 
 const QualityReport = () => {
   return (
     <>
       <BreadCrumb title="Application Quality Report" />
+
+      <Row>
+        <Col lg={4}>
+          <StatusCard />
+        </Col>
+      </Row>
     </>
   );
 };
