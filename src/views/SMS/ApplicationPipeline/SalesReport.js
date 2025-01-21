f3c2e4e7b995e528b9f6dd6bd3867a6ec9be0481
bbmahedi <bluebaymahedi@gmail.com>
2025-01-21 12:43:23 +0600

    `Added custom CSS class "carved-div" and used it in SalesReport component`

--- src/views/SMS/ApplicationPipeline/SalesReport.js
+++ src/views/SMS/ApplicationPipeline/SalesReport.js
@@ -1,10 +1,16 @@
 import React from "react";
 import BreadCrumb from "../../../components/breadCrumb/BreadCrumb";
+import { Card, CardBody } from "reactstrap";
 
 const SalesReport = () => {
   return (
     <>
       <BreadCrumb title="Sales Report" />
+      <Card>
+        <CardBody>
+          <div class="carved-div"></div>
+        </CardBody>
+      </Card>
     </>
   );
 };
