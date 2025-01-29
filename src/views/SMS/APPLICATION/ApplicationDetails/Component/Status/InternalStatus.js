d9857f6f1729fd2447c456e7b7122f0be430144d
bbmahedi <bluebaymahedi@gmail.com>
2025-01-29 12:43:33 +0600

    `Update root URL and add input validation for application status update`

--- src/views/SMS/APPLICATION/ApplicationDetails/Component/Status/InternalStatus.js
+++ src/views/SMS/APPLICATION/ApplicationDetails/Component/Status/InternalStatus.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Select from "react-select";
-import { Col, Form, FormGroup, Row } from "reactstrap";
+import { Col, Form, FormGroup, Input, Row } from "reactstrap";
 import SaveButton from "../../../../../../components/buttons/SaveButton";
 import put from "../../../../../../helpers/put";
 import { useToasts } from "react-toast-notifications";
@@ -13,6 +13,9 @@ const InternalStatus = ({ id, success, setSuccess }) => {
   const [statusDD, setStatusDD] = useState([]);
   const [statusLabel, setStatusLabel] = useState("Select Application Status");
   const [statusValue, setStatusvalue] = useState(0);
+  const [stateMentError, setStateMentError] = useState("");
+  const [stringData, setStringData] = useState(0);
+  const [statement, setStatement] = useState("");
 
   useEffect(() => {
     get(`ApplicationInternalAssesmentRequirement/Statuses`).then((res) => {
@@ -46,9 +49,32 @@ const InternalStatus = ({ id, success, setSuccess }) => {
     value: status?.id,
   }));
 
+  function countWords(str) {
+    const arr = str?.split(" ");
+    return arr?.filter((word) => word !== "")?.length;
+  }
+
+  const handleStringData = (e) => {
+    const count = countWords(e.target.value);
+    setStringData(count);
+    setStatement(e.target.value);
+    if (e.target.value === "") {
+      setStateMentError("Statement is required");
+    } else if (count < 20) {
+      setStateMentError("Statement minimum 20 words");
+    } else {
+      setStateMentError("");
+    }
+  };
+
   const handleApplicationUpdateSubmit = (e) => {
     e.preventDefault();
-
+    if (statusValue === 2 && statement === "") {
+      setStateMentError("Statement is required");
+    }
+    if (statusValue === 2 && stringData < 20) {
+      setStateMentError("Statement minimum 20 words");
+    }
     setProgress(true);
     put(
       `ApplicationInternalAssesmentRequirement/Update/${id}/${statusValue}`
@@ -86,6 +112,26 @@ const InternalStatus = ({ id, success, setSuccess }) => {
                 id="statusId"
               />
             </FormGroup>
+            {statusValue === 2 && (
+              <FormGroup>
+                <span>
+                  Write a note <span className="text-danger">*</span>
+                </span>
+
+                <Input
+                  type="textarea"
+                  name="statement"
+                  id="statement"
+                  value={statement}
+                  onChange={(e) => handleStringData(e)}
+                />
+
+                <div className="d-flex justify-content-between">
+                  <div className="text-danger">{stateMentError}</div>
+                  <div className="text-right">{stringData} / min word-20</div>
+                </div>
+              </FormGroup>
+            )}
 
             <FormGroup>
               <SaveButton text="Save" progress={progress} />
