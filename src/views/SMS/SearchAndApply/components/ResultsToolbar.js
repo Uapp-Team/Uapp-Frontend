5a8c177ae069eddc907c53bde1b792576eea4c79
MohammadSiam <siamk417@gmail.com>
2025-04-09 15:59:34 +0600

    fixed result toolbar styling

--- src/views/SMS/SearchAndApply/components/ResultsToolbar.js
+++ src/views/SMS/SearchAndApply/components/ResultsToolbar.js
@@ -1,6 +1,12 @@
 import React from "react";
-import { FaExchangeAlt, FaHeart, FaSlidersH } from "react-icons/fa";
-import { Button } from "reactstrap";
+import {
+  FaAlignJustify,
+  FaExchangeAlt,
+  FaHeart,
+  FaSlidersH,
+} from "react-icons/fa";
+import courseIcon from "../../../../assets/icon/course.svg";
+import universityIcon from "../../../../assets/icon/university.svg";
 import "../SearchAndApply.css";
 
 const ResultsToolbar = () => {
@@ -8,24 +14,37 @@ const ResultsToolbar = () => {
     <div className="results-toolbar d-flex justify-content-between align-items-center flex-wrap p-2">
       {/* Left Section */}
       <div className="d-flex gap-2 align-items-center flex-wrap">
-        <span className="tag">50 Result</span>
-        <span className="tag tag-active"> 25 Courses</span>
-        <span className="tag"> 25 University</span>
+        <span className="tag mr-2">50 Result</span>
+        <span className="tag tag-active mr-2">
+          <img src={courseIcon} alt="course" className="w-20px h-20px mr-2" />{" "}
+          25 Courses
+        </span>
+        <span className="tag">
+          <img
+            src={universityIcon}
+            alt="course"
+            className="w-20px h-20px mr-2"
+          />
+          25 University
+        </span>
       </div>
 
       {/* Right Section */}
-      <div className="d-flex gap-3 align-items-center flex-wrap">
-        <Button className="action-btn" color="light">
-          <FaHeart className="me-1" /> Favourites{" "}
+      <div className="d-flex align-items-center flex-wrap">
+        <button className="action-btn mr-2">
+          <FaHeart className="mx-2" /> Favourites{" "}
           <span className="count">24</span>
-        </Button>
-        <Button className="action-btn" color="light">
-          <FaExchangeAlt className="me-1" /> Compare{" "}
+        </button>
+        <button className="action-btn mr-2">
+          <FaExchangeAlt className="mx-2" /> Compare{" "}
           <span className="count">5</span>
-        </Button>
-        <Button className="action-btn filters-btn" color="dark">
-          <FaSlidersH className="me-1" /> All filters
-        </Button>
+        </button>
+        <button className="action-btn mr-2">
+          <FaAlignJustify className="my-1" />
+        </button>
+        <button className="action-btn filters-btn">
+          <FaSlidersH className="mx-2" /> All filters
+        </button>
       </div>
     </div>
   );
