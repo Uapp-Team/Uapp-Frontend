c293a9c4d8bc78e54afd56ea6714cd13190ba550
MohammadSiam <siamk417@gmail.com>
2025-04-30 15:05:25 +0600

    fixed dynamic studyMode and durations

--- src/views/SMS/SearchAndApply/components/ApplyCardVar.js
+++ src/views/SMS/SearchAndApply/components/ApplyCardVar.js
@@ -220,18 +220,121 @@ const ApplyCardVar = ({
                               <CiTimer className="mr-2" />
                               <span>Duration</span>
                             </span>
-                            <CustomToolTip methodIds={item.durationNames} />
+                            <span>
+                              {(() => {
+                                const fullTimeDuration = item.durations.find(
+                                  (duration) => Number(duration.studyMode) === 2
+                                );
+                                const otherDurations = item.durations.filter(
+                                  (duration) => Number(duration.studyMode) !== 2
+                                );
+
+                                return (
+                                  <>
+                                    {fullTimeDuration && (
+                                      <span className="duration-tag">
+                                        {fullTimeDuration.name}
+                                      </span>
+                                    )}
+                                    {otherDurations.length > 0 && (
+                                      <Tooltip
+                                        title={
+                                          <div className="custom-tooltip-content">
+                                            <div className="tooltip-header">
+                                              Others
+                                            </div>
+                                            <ul className="tooltip-method">
+                                              {otherDurations.map(
+                                                (method, index) => (
+                                                  <li key={index}>
+                                                    {method.name}
+                                                  </li>
+                                                )
+                                              )}
+                                            </ul>
+                                          </div>
+                                        }
+                                        placement="top"
+                                        overlayClassName="custom-tooltip"
+                                      >
+                                        <InfoCircleOutlined
+                                          style={{
+                                            fontSize: "14px",
+                                            color: "#1890ff",
+                                            cursor: "pointer",
+                                            marginLeft: "4px",
+                                          }}
+                                        />
+                                      </Tooltip>
+                                    )}
+                                  </>
+                                );
+                              })()}
+                            </span>
                           </li>
                           <li className="d-flex justify-content-between">
                             <span>
                               <FaPeopleGroup className="mr-2" />
                               <span>Study Mode</span>
                             </span>
-                            <CustomToolTip
-                              methodIds={item.studyModes}
-                              methods={studyMode}
-                              title="Study Mode"
-                            />
+                            <span>
+                              {(() => {
+                                const methods = item.durations
+                                  ?.map((duration) => {
+                                    return studyMode.find(
+                                      (mode) =>
+                                        mode.id === Number(duration.studyMode)
+                                    );
+                                  })
+                                  .filter(Boolean);
+
+                                const fullTime = methods.find(
+                                  (method) => method.id === 2
+                                );
+                                const others = methods.filter(
+                                  (method) => method.id !== 2
+                                );
+
+                                return (
+                                  <>
+                                    {fullTime && (
+                                      <span className="study-mode-tag">
+                                        {fullTime.name}
+                                      </span>
+                                    )}
+                                    {others.length > 0 && (
+                                      <Tooltip
+                                        title={
+                                          <div className="custom-tooltip-content">
+                                            <div className="tooltip-header">
+                                              Others
+                                            </div>
+                                            <ul className="tooltip-method">
+                                              {others.map((method, index) => (
+                                                <li key={index}>
+                                                  {method.name}
+                                                </li>
+                                              ))}
+                                            </ul>
+                                          </div>
+                                        }
+                                        placement="top"
+                                        overlayClassName="custom-tooltip"
+                                      >
+                                        <InfoCircleOutlined
+                                          style={{
+                                            fontSize: "14px",
+                                            color: "#1890ff",
+                                            cursor: "pointer",
+                                            marginLeft: "4px",
+                                          }}
+                                        />
+                                      </Tooltip>
+                                    )}
+                                  </>
+                                );
+                              })()}
+                            </span>
                           </li>
                           <li className="d-flex justify-content-between">
                             <span>
@@ -262,7 +365,7 @@ const ApplyCardVar = ({
                           [
                             item?.isLoanAvailable && (
                               <span
-                                className="card-tag work-placement mr-1"
+                                className="card-tag work-placement mr-2"
                                 key="loan"
                               >
                                 Loan Available
@@ -270,7 +373,7 @@ const ApplyCardVar = ({
                             ),
                             item?.isWorkPlacementAvailable && (
                               <span
-                                className="card-tag work-placement mr-1"
+                                className="card-tag work-placement mr-2"
                                 key="work-placement"
                               >
                                 Work Placement
