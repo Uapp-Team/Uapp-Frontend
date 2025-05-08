567e0b31d19ce483b4ffc183eacdc58deb4ea19e
Mahedi <bluebaymahedi@gmail.com>
2025-05-08 17:47:26 +0600

    `Update constants.js: switch rootUrl to localtest.uapp.uk`

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,9 @@
 // export const rootUrl = `https://api.uapp.uk/`;
-// export const rootUrl = `https://localtest.uapp.uk/`;
+export const rootUrl = `https://localtest.uapp.uk/`;
 //  export const rootUrl = `https://api.uapp.uk/`;
 // export const rootUrl = `https://localtest.uapp.uk/`;
 // export const rootUrl = `https://lostaquaapple55.conveyor.cloud/`;
-export const rootUrl = `http://192.168.0.136:45456/`;
+// export const rootUrl = `http://192.168.0.136:45456/`;
 
 export const etest = `https://etest.uapp.uk/`;
 //export const etest = `https://192.168.0.138:45455/`;
