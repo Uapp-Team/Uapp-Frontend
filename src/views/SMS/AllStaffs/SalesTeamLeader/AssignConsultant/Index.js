9d6550171bd446bc3504512740b59b4f5a253c97
sadmansakib1997 <sadmansakib670@gmail.com>
2025-06-30 16:55:13 +0600

    sakib

--- src/views/SMS/AllStaffs/SalesTeamLeader/AssignConsultant/Index.js
+++ src/views/SMS/AllStaffs/SalesTeamLeader/AssignConsultant/Index.js
@@ -42,8 +42,8 @@ const Index = () => {
   const [assignCons, setAssignCons] = useState([]);
   const [success, setSuccess] = useState(false);
   const [deleteModal, setDeleteModal] = useState(false);
-  const [salesLeaderName, setSalesLeaderName] = useState("");
-  const [salesLeaderId, setSalesLeaderId] = useState(0);
+  const [consultantName, setConsultantName] = useState("");
+  const [consultantId, setConsultantId] = useState(0);
 
   const [buttonStatus1, setButtonStatus1] = useState(false);
   const [progress1, setProgress1] = useState(false);
@@ -115,17 +115,17 @@ const Index = () => {
   };
 
   const toggleDanger = (p) => {
-    setSalesLeaderId(p?.salesTeamLeaderId);
-    setSalesLeaderName(p?.salesTeamLeaderName);
+    setConsultantId(p?.consultantId);
+    setConsultantName(p?.consultantName);
     setDeleteModal(true);
   };
 
-  const handleDeletePermission = (salesLeaderId) => {
+  const handleDeletePermission = (consultantId) => {
     setButtonStatus1(true);
     setProgress1(true);
 
     remove(
-      `SalesTeamLeader/FetchUnassignedConsultants?salesTeamLeaderId=${salesTeamLeaderId}&salesteamleaderId=${salesLeaderId}`
+      `SalesTeamLeader/UnassignConsultantsToLeader?salesTeamLeaderId=${salesTeamLeaderId}&consultantId=${consultantId}`
     ).then((action) => {
       setButtonStatus1(false);
       setProgress1(false);
@@ -135,8 +135,8 @@ const Index = () => {
         appearance: "error",
         autoDismiss: true,
       });
-      setSalesLeaderId(0);
-      setSalesLeaderName("");
+      setConsultantId(0);
+      setConsultantName("");
     });
   };
 
@@ -267,14 +267,14 @@ const Index = () => {
       </Card>
 
       <ConfirmModal
-        text={`Do You Want To Delete This ${salesLeaderName} Information ?`}
+        text={`Do You Want To Delete This ${consultantName} Information ?`}
         isOpen={deleteModal}
         toggle={closeModal}
-        confirm={() => handleDeletePermission(salesLeaderId)}
+        confirm={() => handleDeletePermission(consultantId)}
         cancel={() => {
           setDeleteModal(false);
-          setSalesLeaderId(0);
-          setSalesLeaderName("");
+          setConsultantId(0);
+          setConsultantName("");
         }}
         buttonStatus={buttonStatus1}
         progress={progress1}
